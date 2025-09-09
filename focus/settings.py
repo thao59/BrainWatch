609f7d1cab778a9cc7527313423f123d6c3344aa
Thao Dinh <dinh.thao0509@gmail.com>
2025-09-09 21:58:55 +1000

    add path to static

--- focus/settings.py
+++ focus/settings.py
@@ -112,11 +112,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+import os 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "brainwatch", "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
