8c4ee9c7f8375932fab48abace75b8cf73820013
Thao Dinh <dinh.thao0509@gmail.com>
2025-09-10 16:07:36 +1000

    testing allowed hosts

--- focus/settings.py
+++ focus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!ko*40-3wft^x=0@+p=xo4a_#o@bewhnz8+6l$3fmc4zhq8r%j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['brainwatch-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
